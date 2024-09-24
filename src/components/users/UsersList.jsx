9b3c6f4b090ab6d54ae0eaf720f712763765fb4d
visheshtomarr <visheshtomar7800@gmail.com>
2024-09-24 19:45:59 +0530

    Added UsersList component to fetch Github profiles

---
+++ src/components/users/UsersList.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function UsersList() {
+    // Declare users and set-state method for users.
+    const [users, setUsers] = useState([]);
+
+    // Delcare loading state and set-state method for users.
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    // Function to fetch users from the GITHUB API.
+    const fetchUsers = async () => {
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        });
+
+        const data = await response.json();
+        setUsers(data);
+        setLoading(false);
+    }
+
+    if(!loading) {
+        return (
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                {users.map((user) => (
+                    <h3>{user.login}</h3>
+                ))}
+            </div>
+        )
+    } else {
+        return <h3>Loading...</h3>
+    }
+    
+}
+
+export default UsersList;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import UsersList from "../components/users/UsersList";
+
 function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Welcome</h1>
+      <UsersList />
     </div>
   )
 }
